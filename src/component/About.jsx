67b555f078009037fe59bb68b0ed4990672e8916
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-09-21 14:08:50 +0100

    my portfolio

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
                 <p className='font-semibold mt-3 mb-5'>I am a passionate fullstack developer with a strong background in creating both dynamic and non-dynamic websites using advanced HTML, CSS, Javascript, Node js, and React. With a relentless commitment to excellence, I do not give up when  tackling challenges and finding innovative solutions. My skill set, joined with a love for continuous learning, allows me to triumph in dynamic environments and contribute to meaningful projects.</p>
                 <span>
                     <a href="" className='border p-2 rounded bg-pink-700 font-bold  hover:bg-white hover:text-pink-700'>See my work</a>
-                    <a href="" className='mx-5 border p-2 rounded bg-white text-pink-700  font-bold hover:bg-pink-700 hover:text-white'>Download CV</a>
+                    <a href="https://drive.google.com/file/d/1cnjU4Lq7zw3utvnDbZ7rmb6-5hVffaeN/view?usp=sharing" className='mx-5 border p-2 rounded bg-white text-pink-700  font-bold hover:bg-pink-700 hover:text-white'>Download CV</a>
                 </span>
             </div>
             <div className='lg:ms-10 sm:py-10 px-10' data-aos="fade-down">
