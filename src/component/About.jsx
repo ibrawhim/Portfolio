596da1e78727096f08444d54c01f434d6e1ff044
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-11-22 22:13:17 +0100

    movie

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
                     />
                 </h2>
                 <h6 className='my-3 text-xl font-bold'><span className='font-bold me-2'>--------</span>About me:</h6>
-                <p className='font-semibold mt-3 mb-5'>I am a passionate fullstack developer with a strong background in creating both dynamic and non-dynamic websites using advanced HTML, CSS, Javascript, Node js, and React. With a relentless commitment to excellence, I do not give up when  tackling challenges and finding innovative solutions. My skill set, joined with a love for continuous learning, allows me to triumph in dynamic environments and contribute to meaningful projects.</p>
+                <p className='font-semibold mt-3 mb-5'>I am a passionate fullstack developer with a strong background in creating both dynamic and non-dynamic websites using advanced HTML, CSS, Javascript, Node js, and React. With a relentless commitment to excellence, I do not give up when  tackling challenges and finding innovative solutions. My skill set, joined with a love for continuous learning, allows me to triumph in dynamic environments and contribute to meaningful projects..</p>
                 <span>
                     <Link  to='portfolio' className='border p-2 rounded bg-pink-700 font-bold  hover:bg-white hover:text-pink-700'>See my work</Link>
                     <a href="https://drive.google.com/file/d/1cnjU4Lq7zw3utvnDbZ7rmb6-5hVffaeN/view?usp=sharing" className='mx-5 border p-2 rounded bg-white text-pink-700  font-bold hover:bg-pink-700 hover:text-white'>Download CV</a>
