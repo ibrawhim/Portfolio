468b1c58a94c53809e97ee960d8b9882074f706e
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2024-01-11 09:17:40 +0100

    clients

--- src/component/ProjectList.jsx
+++ src/component/ProjectList.jsx
@@ -12,7 +12,7 @@ export const project = [
         name: 'movie-app',
         info: 'This is an movie app. It enables users to check new released and unreleased genres of movies',
         tools: 'HTML, CSS, Tailwind and React.',
-        img: '/cloud.jpg',
+        img: '/cloud.png',
         link: 'https://ibrahim-movie-app.netlify.app/',
         color: 'text-white',
         bg: 'bg-pink-700'
