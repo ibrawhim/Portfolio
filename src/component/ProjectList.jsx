4f6a00cc1b5167cfab3c818b28138ea964f024b8
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2024-01-11 09:45:07 +0100

    clients

--- src/component/ProjectList.jsx
+++ src/component/ProjectList.jsx
@@ -1,14 +1,5 @@
 export const project = [
     {
-        name: 'Anonymous Message App',
-        info: 'This enables user to send their anonymous message link to users so they can receive message anonymously from people without knowing or finding out the sender',
-        tools: 'HTML, CSS, Bootstrap and Javascript.',
-        img: '/new.jpg',
-        link: 'https://regal-dasik-41ecda.netlify.app/',
-        bg: 'bg-white',
-        color: 'text-pink-700',
-    },
-    {
         name: 'movie-app',
         info: 'This is an movie app. It enables users to check new released and unreleased genres of movies',
         tools: 'HTML, CSS, Tailwind and React.',
@@ -18,8 +9,17 @@ export const project = [
         bg: 'bg-pink-700'
     },
     {
+        name: 'Anonymous Message App',
+        info: 'This enables user to send their anonymous message link to users so they can receive message anonymously from people without knowing or finding out the sender',
+        tools: 'HTML, CSS, Bootstrap and Javascript.',
+        img: '/new.jpg',
+        link: 'https://regal-dasik-41ecda.netlify.app/',
+        bg: 'bg-white',
+        color: 'text-pink-700',
+    },
+    {
         name: 'School Portal',
-        info: 'A school portal where student can sign-up, have a person matric number and check out their various dashboard',
+        info: 'A school portal where student can sign-up, have a personal matric number and check out their various dashboards',
         tools: 'React, Taiwind, Node js, Express and MongoDB',
         img: '/fully.jpg',
         link: 'https://timely-conkies-c49131.netlify.app/',
@@ -27,11 +27,11 @@ export const project = [
         color: 'text-pink-700'
     },
     {
-        name: 'Salesforce',
-        info: 'A clone of Salesforce webapp',
-        tools: 'HTML, CSS and  Bootstrap',
-        img: '/Salesforce.png',
-        link: 'https://sales-pi.vercel.app/',
+        name: 'Game-Plan',
+        info: 'A Gameplan form, where users can select different gameplans and make a subscription after',
+        tools: 'HTML, CSS, Tailwind and  React ',
+        img: '/game.jpeg',
+        link: 'https://game-plan.netlify.app/',
         color: 'text-white',
         bg: 'bg-pink-700'
     },
